d7d1cccbcdcb721c397009f940591bd661f54063
chrismountzouris <30795982+chrismountzouris@users.noreply.github.com>
2021-01-31 23:08:56 +0200

    Fixes on JSON Object
    Handle JSON object as Python dictionary

--- covidVacs.py
+++ covidVacs.py
@@ -1,6 +1,11 @@
+# Import libraries
 import requests
+
 import json
 
+import datetime
+
+# Initialize variables
 def jprint(obj):
 
     text = json.dumps(obj, sort_keys=True, indent=4)
@@ -13,14 +18,12 @@ def jload(obj):
 
     return loaded_json
     
-
+# A function that returns the JSON request respond
 def get_vacs_json():
 
     response = requests.get("https://data.gov.gr/api/v1/query/mdg_emvolio", headers={'Authorization': 'Token d99450c9e2d9654b8b911ffb8d33f9a6291e95f8'})
 
     if (response.status_code == 200):
-
-        print ("Successful Request")
 
         return response.json()
 
@@ -30,8 +33,32 @@ def get_vacs_json():
 
         return None
 
+# Initialize variables
+total_distinct_vacs = 0
+
+total_non_distinct_vacs = 0
+
+# Request JSON object and turn it into a Python dictionary
 vacs_json = get_vacs_json()
 
 formatted_json = jprint(vacs_json)
 
 loaded_json = jload(vacs_json)
+
+# Get previous day string
+now = datetime.date.today() - datetime.timedelta(1)
+
+time_compare_string = now.strftime("%Y-%m-%d") + 'T00:00:00'
+
+# Iterate through dictionary and perform actions on the last day's data
+for key in loaded_json:
+    
+    if (key['referencedate'] == time_compare_string):
+        
+        total_distinct_vacs = total_distinct_vacs + int(key['totaldistinctpersons'])
+
+        total_non_distinct_vacs = total_non_distinct_vacs + int(key['totalvaccinations'])
+
+print ("Total distinct vaccinations are :",total_distinct_vacs)
+
+print ("Total vaccinations are :",total_non_distinct_vacs)
